df2fddf05b224c042b1bb8a0b63ab1fdb6c2e6da
heng michel <cavacmoi@gmail.com>
2023-06-20 10:50:10 +0200

    addExerciceJS

---
+++ Exercice/js/12_manege.js
@@ -0,0 +1,36 @@
+// var nbTour = prompt(" Combien de tour souhaitez vous faire ?");
+// var result = nbTour + "  ";
+// while (nbTour < 10) {
+//   nbTour++;
+
+//   document.write("" + "C'est le tour de manège n°" + result);
+// }
+
+var nbTour = prompt(" Combien de tour souhaitez vous faire ?");
+
+if(nbTour>10 )
+document.write("" + " Pas plus de 10 tours");
+else if(nbTour<=0)
+alert("" + " Error");
+document.write("Recommence");
+  document.write("" + "C'est le tour de manège n°" + nbTour + "<br/>");
+
+// var nbTour = prompt(" Combien de tour souhaitez vous faire ?");
+// var result = 0;
+// while (result < nbTour) {
+//   result++;
+
+//   document.write("" + "C'est le tour de manège n°" + result + "<br/>");
+// }
+// document.write(result);
+
+//     if(nbTour===1){
+//         nbTour= (nbTour + "C'est le tour de manège n°1");
+//         nbTour++;
+
+//     if(nbTour>1 && nbTour===2){
+//         nbTour= (nbTour + "C'est le tour de manège n°2");
+//         nbTour++;
+//     }
+
+// }
