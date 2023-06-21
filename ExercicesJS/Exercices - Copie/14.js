088d6a61a91061125b21ab499feafa1d96cbf855
heng michel <cavacmoi@gmail.com>
2023-06-21 14:02:52 +0200

    ex14js

--- ExercicesJS/Exercices - Copie/14.js
+++ ExercicesJS/Exercices - Copie/14.js
@@ -1,7 +1,7 @@
-var nombre = 0; // Valeur initiale permettant d'entrer dans la boucle
-while ((nombre < 50) || (nombre > 100)) {
-    nombre = Number(prompt("Entrez un nombre entre 50 et 100 : "));
-}
+// var nombre = 0; // Valeur initiale permettant d'entrer dans la boucle
+// while ((nombre < 50) || (nombre > 100)) {
+//     nombre = Number(prompt("Entrez un nombre entre 50 et 100 : "));
+// }
 
 
 // premier methode sans inverse ne fonctionne pas?
@@ -16,16 +16,24 @@ while ((nombre < 50) || (nombre > 100)) {
 //     nombre = prompt("Entrer un nombre entre 50 et 100 :");
 // }
 // troisieme methode avec boucle while et dedans if else ne fonctionne pas?
-// while (nombre != 'quitter') {
-//     if (nombre >= 50 && nombre <= 100) {
-//         alert("le nombre : " + nombre + " que Vous avez saisie  est compris entre 50 et 100");
-//         nombre = prompt("Entrer un nombre  'quitter' pour quitter");
-//     }
-//     else if (nombre < 50 || nombre > 100) {
-//         alert("Vous avez saisie " + nombre + " n'appartient pas a l'intervalle 50 et 100");
-//         nombre = prompt("Entrer un nombre  'quitter' pour quitter");
-//     }
-//     else {
-//         alert("Erreure entrer un nombre correcte");
-//     }
-// }
+
+
+   var nombre = Number(prompt("Entrez un nombre entre 50 et 100 : "));
+
+while (nombre != 'quitter') {
+    if (nombre >= 50 && nombre <= 100) {
+        alert("le nombre : " + nombre + " que Vous avez saisie  est compris entre 50 et 100");
+        nombre = prompt("Entrer un nombre  'quitter' pour quitter");
+    }
+    else if (nombre < 50 || nombre > 100) {
+        alert("Vous avez saisie " + nombre + " n'appartient pas a l'intervalle 50 et 100");
+        nombre = prompt("Entrer un nombre  'quitter' pour quitter");
+    }
+    else {
+        alert("Erreur entrer un nombre correcte");
+    }
+}
+
+
+    
+
