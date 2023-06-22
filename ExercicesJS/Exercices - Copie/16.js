7943f0cbaaac0d1ccc26545c46c23a7b16efbf4b
heng michel <cavacmoi@gmail.com>
2023-06-22 13:33:50 +0200

    addEx16bis

--- ExercicesJS/Exercices - Copie/16.js
+++ ExercicesJS/Exercices - Copie/16.js
@@ -1,12 +1,25 @@
 // alert('test');
 
-var disMoi = "";
-var i = 1;
+// var disMoi = "";
+// var i = 1;
 
-while ((disMoi !== "oui") && (disMoi !== "non") && (disMoi !== "Oui") && (disMoi !== "Non") && (disMoi !== "OUI") && (disMoi !== "NON")) {
-    disMoi = prompt("Allez!! On joue à ni oui ni non?");
-    document.write('"' + disMoi + '" n\'est pas une réponse, pour la ' + i + '° fois, veux-tu jouer oui ou non? <br>');
-    i++;
+// while ((disMoi !== "oui") && (disMoi !== "non") && (disMoi !== "Oui") && (disMoi !== "Non") && (disMoi !== "OUI") && (disMoi !== "NON")) {
+//     disMoi = prompt("Allez!! On joue à ni oui ni non?");
+//     document.write('"' + disMoi + '" n\'est pas une réponse, pour la ' + i + '° fois, veux-tu jouer oui ou non? <br>');
+//     i++;
+// }
+// i--;//parce qu'il y a une incrémentation en trop
+// document.write("<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " + i + " tours.</p>");
+
+var disMoi = "";
+var i = 0;
+// ou var i = 1; il faut désincrémenté avec i-- après la boucle
+while ((disMoi!== "OUI" && disMoi!=="oui") && (disMoi !== "NON" && disMoi!= "non") 
+ ) {
+    disMoi = prompt("Nous allons jouer à ni oui ni non ?");
+    disMoiLowercase = disMoi.toLocaleLowerCase();
+    document.write('"' + disMoiLowercase + '" n\'est pas une réponse, pour la ' + i + '° fois, veux-tu jouer oui ou non? <br>');
+     i++;
 }
-i--;//parce qu'il y a une incrémentation en trop
+// i--;
 document.write("<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " + i + " tours.</p>");
---
+++ ExercicesJS/Exercices - Copie/16Bis.js
@@ -0,0 +1,52 @@
+// alert('test');
+
+var disMoi = "";
+alert("dismoi A: " + disMoi);
+var i = 1;
+alert("i A: " + i);
+
+// (disMoi !== "oui" || disMoi !== "non") &&
+//   (disMoi !== "Oui" || disMoi !== "Non") &&
+//   (disMoi !== "OUI" || disMoi !== "NON");
+
+// (disMoi !== "oui" ||
+//   disMoi !== "non" ||
+//   disMoi !== "Oui" ||
+//   disMoi !== "Non" ||
+//   disMoi !== "OUI" ||
+//   disMoi !== "NON");
+
+while (
+  disMoi !== "oui" &&
+  disMoi !== "non" &&
+  disMoi !== "Oui" &&
+  disMoi !== "Non" &&
+  disMoi !== "OUI" &&
+  disMoi !== "NON"
+) {
+  disMoi = prompt("Allez!! On joue à ni oui ni non?");
+
+  alert("dismoi B: " + disMoi);
+  document.write(
+    '"' +
+      disMoi +
+      "\" n'est pas une réponse, pour la " +
+      i +
+      "° fois, veux-tu jouer oui ou non? <br>"
+  );
+
+  alert("i B: " + i);
+  i++;
+
+  alert("i C: " + i);
+}
+
+alert("i D: " + i);
+i--;
+
+alert("i E: " + i); //parce qu'il y a une incrémentation en trop
+document.write(
+  "<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " +
+    i +
+    " tours.</p>"
+);
