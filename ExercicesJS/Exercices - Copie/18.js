69451ca1db922b9cb5c772700e4c2fea620978dd
heng michel <cavacmoi@gmail.com>
2023-06-23 16:49:05 +0200

    exo18.js

--- ExercicesJS/Exercices - Copie/18.js
+++ ExercicesJS/Exercices - Copie/18.js
@@ -1,7 +1,24 @@
 // Solution utilisant une série de if/else
-for (var nombre = 1; nombre <= 100; nombre++) {
-    if ((nombre % 3 === 0) && (nombre % 5 === 0)) console.log("FizzBuzz");
-    else if (nombre % 3 === 0) console.log("Fizz");
-    else if (nombre % 5 === 0) console.log("Buzz");
-    else console.log(nombre);
+// for (var nombre = 1; nombre <= 100; nombre++) {
+//     if ((nombre % 3 === 0) && (nombre % 5 === 0)) console.log("FizzBuzz");
+//     else if (nombre % 3 === 0) console.log("Fizz");
+//     else if (nombre % 5 === 0) console.log("Buzz");
+//     else console.log(nombre);
+// }
+for (var number = 1; number<=100;number++){
+    if 
+    ((number%3=== 0)&& ( number%5=== 0)){
+       document.write("FizzBuzz <br>");
+    
+    }else
+if ( number%3=== 0){
+document.write("Fizz <br>" ); 
+}
+else if
+    ( number%5=== 0){
+        document.write("Buzz <br>" );
+        
+    }else{
+    document.write(number + "<br>");
+ }
 }
