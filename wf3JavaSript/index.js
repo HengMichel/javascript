2580b75c085ac90140d0cd5acb2547ca33944f5c
heng michel <cavacmoi@gmail.com>
2023-06-28 16:33:51 +0200

    wf3JavaScriptUpdate2

--- wf3JavaSript/index.js
+++ wf3JavaSript/index.js
@@ -82,22 +82,265 @@
 
 // }
 
+// ***************************************** exo J-2 *******************************************************
 
-// en ulisant la ternaire ecrire un programe qui affiche adulte si l'age est superieur ou egal a  18 et mineur si age est inferieur a 18
+// (4 == 4) && (5 != "5")              // => false
+// (10 > 5) || (3 <= 2)                // => true
+// !(true && false)                    // => true
+// (4 >= 4) && !(false || true)        // => false
+// (7 >= 8) || !(false && true)        // => true
+// (5 > 3) && (10 == 10)               // => true
+// !(true || false) && (6 >= 6)        // => false
+// (8 <= 4) || !(false && true)        // => true
+// !((10 > 8) || (5 < 2))              // => false
 
-var age = 17;
-var personne = (age<18)?"mineur !" : " mageur !";
-console.log(personne);
+ 
 
-// une annee bisextile est une annee qui est soit:
-// 1 - divisible par 4 sans etre divisible par 100
-// soit:
-// 2 - divisible par 400
 
-var years = 365;
-if(years%4===0 && years!==0 || years%400===0){
-    // return true;
+// let i = 6; 
+
+
+// do {
+//     console.log(i); // affiche 6
+//     i++;  // incremente i 6 + 1 = 7
+// } while (i < 5); // verifie la condition
+
+ 
+
+// console.log(i); // affiche 7
+
+ 
+
+
+// // exemple du break => arreter l'execution de la boucle si la variable i egale 4
+// for (let i = 0; i < 10; i++){
+//     console.log(i); // affiche i
+//     if (i == 4) { // si i = 4
+//         break; // sortir de la boucle
+//     }
+// }
+
+ 
+
+// //  exemple du continue => on souhaite afficher que les nombres pairs entre 0 et 10
+// for (let i = 0; i <= 10; i++){
+//     if (i % 2 != 0) { // verifie si i n'est pas un nombre pair
+//         continue; // ignore et passe a l'iteration suivante
+//     }
+//     console.log(i);
+// }
+
+ 
+
+// // idem
+// for (let i = 0; i <= 10; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+ 
+
+// //  exemple du continue => on souhaite afficher que les nombres impairs entre 0 et 10
+// for (let i = 0; i <= 10; i++){
+//     if (i % 2 == 0) { // verifie si i n'est pas un nombre impair
+//         continue; // ignore et passe a l'iteration suivante
+//     }
+//     console.log(i);
+// }
+
+
+// let tab = ["Alin", 45]; // ["Alin", 45]
+// tab[3] = "2 rue de l'amour"; // ["Alin", 45, "", "2 rue de l'amour"]
+// // vue que le tableau n'a pas d'indice 2 et 3 met l'indice 2 a vide et l'indice 3 avec la valeur
+// console.log(tab);
+// tab.push("0678545855"); // ["Alin", 45, "", "2 rue de l'amour", "0678545855"]
+// console.log(tab);
+// tab.unshift("Johanne"); // ["Johanne", "Alin", 45, "", "2 rue de l'amour", "0678545855"]
+// console.log(tab);
+// tab[3] = "future"; // ["Johanne", "Alin", 45, "future", "2 rue de l'amour", "0678545855"]
+// console.log(tab);
+// delete tab[3]; // ["Johanne", "Alin", 45, "", "2 rue de l'amour", "0678545855"]
+// console.log(tab);
+// tab = new Array(); // reinitialise le tableau
+// tab = [];  // reinitialise le tableau
+
+ 
+
+// let tableau = ["Cynthia", "Narcis", "Rahim", "Nawal", "Karima"];
+// let index = tableau.indexOf("Narcis");
+// console.log(index); // 1
+// let newTab = tableau.slice(0, 3); // ["Cynthia", "Narcis", "Rahim"]
+// console.log(newTab); 
+
+ 
+
+// let tabNumer = [1, 8, 19, 5];
+// console.log(tabNumer.sort()); // [1, 19, 5, 8]
+
+ 
+
+// tabNumer.sort(function (a, b) {
+//     return a - b;
+// });
+
+ 
+
+// let tab2 = [2, 7, 9, 8];
+// tab2.fill("Alexis"); // ["Alexis", "Alexis", "Alexis", "Alexis"]
+// console.log(tab2);
+// tab2.fill("Michel", 1, 3); // ["Alexis", "Michel", "Michel", "Alexis"]
+// console.log(tab2);
+
+ 
+
+// let tab3 = ["Alexis", "Jérémy", "Michel", "Faissal"];
+// let str = tab3.join(' '); // "Alexis Jérémy Michel Faissal"
+// console.log(str);
+// console.log(tab3.reverse()); // ["Faissal", "Michel", "Jérémy", "Alexis"];
+
+ 
+
+// let tab4 = [2, 5, 8];
+// // tab4.splice(1, 2); // [2]
+// tab4.splice(2, 0, 'a', 'b'); // [2, 5, 'a', 'b', 8]
+// console.log(tab4);
+
+ 
+
+// let tab5 = [1, 2, 3];
+// let tab6 = [4, 5, 6];
+// let tab7 = tab5.concat(tab6); // [1, 2, 3, 4, 5, 6]
+// console.log(tab7); 
+
+ 
+
+// let tab8 = [1, 6, 8, 3];
+// let tab9 = tab8.map(e => e * 5); // [5, 30, 40, 15]
+// console.log(tab9);
+
+
+// *********************************************** Exercice  ****************************************************
+// Exercise
+// be the table: ["banana", "apple", "mango"]
+// Add a strawberry between apple and mango
+// Remove the bannan
+// replace strawberry and mango and apple by guava
+
+
+// tableau.splice(2, 0, 'a', 'b'); // Ajoute 'a' et 'b' à partir de l'indice 2
+// tableau.shift(); // Supprime le premier élément du tableau
+// tableau.fill(0, 1, 4); // les éléments de l'indice 1 à l'indice 3 sont remplacés
+// par la valeur 0
+
+// let fruit = ["banane", "pomme" , "mangue"];
+// fruit.splice(2,0,"fraise");
+// console.log(fruit);
+// fruit.shift();
+// console.log(fruit);
+// fruit.fill("goyave");
+// console.log(fruit);
+
+
+// ************************************* Fonction  *****************************************************************
+
+function maxMin(a,b,c) {
+    if(c == true){
+        if(a>b){
+            console.log(a);
+            return a;
+        }
+        else if(b>a){
+            console.log(b);
+            return b;
+        }
+        
+    }
+    else if(c == false){
+        if(a<b){
+            console.log(a);
+            return a;
+        }
+        else if (b<a){
+            console.log(b);
+            return b;
+        }
+    }
 
 }
-// else return false;
 
+let a = 10;let b = 20;
+
+let result1 = maxMin(10,20,true);
+let result2 = maxMin(10,20,false);
+
+
+// ************************************ Correction ************************************************************
+function addition(a, b) {
+    let resultat = a + b;
+    return resultat;
+   }
+   // idem
+   function addition(a, b) {
+    return a + b;
+   }
+    
+   // let somme = addition(2, 5);
+   // let somme2 = addition(4, 7);
+   // console.log(somme);
+   // console.log(somme2);
+    
+   function direBonjourPrenom(prenom = "Anonyme") {
+    document.write("Bonjour, ", prenom, " !");
+   }
+    
+   direBonjourPrenom(); // Bonjour, Anonyme !
+    
+   // direBonjourPrenom("Michel"); // Bonjour, Michel !
+    
+   // Exo 1
+   function maxMin(a, b, c = true) {
+    if (c == true) { // si c == true
+    if (a > b) { // si a plus grand que b
+    return a; // on retourne a
+    } else { // sinon
+    return b; // on retourne b
+    }
+    } 
+    else { // si c = false
+    if (a < b) { // si a est plus petit que b
+    return a; // on retourne a
+    } else { // sinon
+    return b; // on retourne b
+    }
+    }
+   }
+   let resultat = maxMin(3, 8, true);
+   console.log(resultat);
+    
+   // on retourne a si (c = true et a > b) ou (c = false et a < b)
+   // on retourne b si (c = true et a < b) ou (c = false et a > b)
+   function maxMin(a, b, c = true) {
+    if (((c == true) && (a > b)) || ((c == false) && (a < b))) {
+    return a;
+    } else if (((c == true) && (a < b)) || ((c == false) && (a > b))) {
+    return b;
+    }
+   }
+    
+   let result = maxMin(3, 8, false);
+   console.log(result);
+
+//    ************************************************** another excercice ****************************************
+
+// ecrire une fonction sommeTab qui prend en paramètre un tableau composé de nombres.
+// la fonction doit retourner la somme des éléments du tableau 
+// exemple : sommeTab([1,6,9]) =>16
+
+function sommeTab(a,b,c){
+let result3 = a+b+c;
+
+return result3 
+}
+let somme = sommeTab(1,6,9);
+console.log(somme);
